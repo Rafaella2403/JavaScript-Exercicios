788df4e2a559ce60ab0659001749ca24ac3d84b2
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-04-06 15:09:39 -0300

    Resolvendo exercícios

--- Object/script.js
+++ Object/script.js
@@ -1,12 +1,32 @@
 // Crie uma função que verifique corretamente o tipo de dado
+function verificarDado(dado){
+  return Object.prototype.toString.call(dado);
+}
+
+console.log(verificarDado('Digite o dado'));
 
 // Crie um objeto quadrado com a propriedade lados e torne ela imutável
+const quadrado = {};
+Object.defineProperties(quadrado, {
+  lados: {
+    value: 4,
+     enumerable: true,
+  }
+});
+
+console.log(quadrado);
 
 // Previna qualquer mudança no objeto abaixo
 const configuracao = {
     width: 800,
     height: 600,
     background: '#333'
-  }
+}
+
+Object.freeze(configuracao);
+
+console.log(configuracao);
   
-  // Liste o nome de todas as propriedades do protótipo de String e Array
+// Liste o nome de todas as propriedades do protótipo de String e Array
+console.log(Object.getOwnPropertyNames(String.prototype));
+console.log(Object.getOwnPropertyNames(Array.prototype));
