8033be5b1b0d5f2a2cdccc2dc57b8a70b73e3775
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-04-06 11:08:55 -0300

    Resolvendo Exercícios

--- ArrayeInteracoes/script.js
+++ ArrayeInteracoes/script.js
@@ -1,16 +1,38 @@
-// Selecione cada curso e retorne uma array
-// com objetos contendo o título, descricao,
-// aulas e horas de cada curso
+/*Selecione cada curso e retorne uma array
+com objetos contendo o título, descricao,
+aulas e horas de cada curso*/
 
+const cursos = document.querySelectorAll('.cursos');
+const arrayCursos  = Array.from(cursos);
 
-// Retorne uma lista com os
-// números maiores que 100
+const objetosCurso = arrayCursos.map(curso => {
+    const titulo = curso.querySelector('h1').innerText;
+    const descricao = curso.querySelector('p').innerText;
+    const aulas = curso.querySelector('.aulas').innerText;
+    const horas = curso.querySelector('.horas').innerText;
+    return{
+        titulo,
+        descricao,
+        horas,
+        aulas
+    }
+});
+
+console.log(arrayCursos);
+console.log(objetosCurso);
+
+// Retorne uma lista com os números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maiores = numeros.filter(n => n > 100);
+console.log(maiores);
 
 
-// Verifique se Baixo faz parte
-// da lista de instrumentos e retorne true
+// Verifique se Baixo faz parte da lista de instrumentos e retorne true
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const possui = instrumentos.some(item => {
+    return item === 'Baixo'
+})
+console.log(possui);
 
 
 // Retorne o valor total das compras
@@ -36,3 +58,12 @@ const compras = [
     preco: 'R$ 10,60'
   }
 ]
+
+const valorTotalCompras = compras.reduce((acumulador, item) => {
+
+    const precoLimpo = +item.preco.replace('R$', '').replace(',', '.');
+    return acumulador + precoLimpo;
+
+}, 0);
+
+console.log(valorTotalCompras);
